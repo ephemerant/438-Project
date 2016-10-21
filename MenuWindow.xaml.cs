72c6ec6d4c875769292f6a9d6837f94e795a5353
cmorphis <horizonsfirst@gmail.com>
2016-10-21 18:10:47 -0400

    Starting to add the menu, not done yet
    I'm just pushing this as it pops up and doesn't  break the game yet. I'm
    still working on it today.

---
+++ MenuWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.IO;
+
+namespace UNO
+{
+    public partial class MenuWindow : Window
+    {
+        string resourcesPath;
+        string imagesPath;
+
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+        void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // First path is for general distribution, second is for when debugging
+            string[] possibleDirectories = { @"resources", @"..\..\resources" };
+
+            foreach (var dir in possibleDirectories)
+                if (Directory.Exists(dir))
+                {
+                    resourcesPath = Path.GetFullPath(dir);
+                    imagesPath = Path.Combine(resourcesPath, "cards");
+                    break;
+                }
+
+        }
+    }
+}
